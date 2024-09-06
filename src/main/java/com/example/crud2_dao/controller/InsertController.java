2a5772ce4f42c8bf206aa0ffe9598e21453e1c83
knowledgemoor <gabrieldu1180@gmail.com>
2024-09-06 04:54:41 -0300

    Refactor(SonarLint) : fixed 'Unnecessary imports should be removed' SonarLint rule (java:S1128)

--- src/main/java/com/example/crud2_dao/controller/InsertController.java
+++ src/main/java/com/example/crud2_dao/controller/InsertController.java
@@ -5,14 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.crud2_dao.dao.CrudDao;
 import com.example.crud2_dao.domain.Consulta;
-import com.example.crud2_dao.domain.Medico;
-import com.example.crud2_dao.domain.Pacientes;
-import com.example.crud2_dao.controller.CrudController;
+
 
 @Controller
 public class InsertController {
